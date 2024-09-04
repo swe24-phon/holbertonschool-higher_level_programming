d2612f47060fd95c490ef1e3cd3e511358e3d7ed
phon xay <majic42@hotmail.com>
2024-09-04 19:57:10 +1000

    Fix logic

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,5 +5,7 @@ def fizzbuzz():
             print("FizzBuzz")
         elif i % 3 == 0:
             print("Fizz")
-        else:
+        elif i % 5 == 0:
             print("Buzz")
+        else:
+            print(i)
