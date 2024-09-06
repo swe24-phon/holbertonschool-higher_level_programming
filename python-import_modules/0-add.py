7768cc9f3787106bd7db472cafa289f35e07e9fa
phon xay <majic42@hotmail.com>
2024-09-06 17:30:36 +1000

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 from add_0 import add
 
 def main():
-    print("{} + {} = {}".format(a, b, add(a,b))
+    print("{} + {} = {}".format(a, b, add(a = 1,b = 2))
 
 if __name__ == "__main__":
     main()
