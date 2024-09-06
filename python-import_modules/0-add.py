86a3c3954f900fe508582e4906d56abff4789f75
phon xay <majic42@hotmail.com>
2024-09-06 17:26:51 +1000

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,10 +2,7 @@
 from add_0 import add
 
 def main():
-    a = 1
-    b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print(f"{a} + {b} = {add(a,b)}")
 
 if __name__ == "__main__":
     main()
