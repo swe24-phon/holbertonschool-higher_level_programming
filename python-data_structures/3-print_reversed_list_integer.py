736e6bd654517fb988589fd6334576a18ec9dcc4
phon xay <majic42@hotmail.com>
2024-09-10 17:55:52 +1000

    first commit

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """
+    Prints all integers in the list, in reverse order.
+    
+    Parameters:
+    my_list (list): The list to modify
+
+    Returns:
+    None
+    
+    """
+
+for i in my_list:
+    print("{}".format(my_list[len(my_list - i)]))
