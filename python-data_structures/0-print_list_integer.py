15360ecd762f242033f0751b3e6b444866880db1
phon xay <majic42@hotmail.com>
2024-09-10 16:22:32 +1000

    modify code

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """Prints the first integer in the list; if the list is empty it prints None."""
-    if len(my_list) == 0:
-        print(None)
+    if my_list is None:
+        print(" ")
     else:
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+        for integer in my_list:
+            print("{:d}".format(my_list[integer]))
