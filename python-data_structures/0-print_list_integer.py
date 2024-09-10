a51314b287813d83302503d42f70df88c7613273
phon xay <majic42@hotmail.com>
2024-09-10 16:38:31 +1000

    fix description

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """
-    Prints the first integer in the list; if the list is
+    Prints the integer in the list; if the list is
     empty it prints None.
     """
     if my_list is None:
