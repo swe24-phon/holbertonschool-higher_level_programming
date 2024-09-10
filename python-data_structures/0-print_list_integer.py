82927b64a7f0d8ab7e8642aa846f09ee1b0193ba
phon xay <majic42@hotmail.com>
2024-09-10 16:24:48 +1000

    modify code 2

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,5 +4,5 @@ def print_list_integer(my_list=[]):
     if my_list is None:
         print(" ")
     else:
-        for integer in my_list:
-            print("{:d}".format(my_list[integer]))
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
