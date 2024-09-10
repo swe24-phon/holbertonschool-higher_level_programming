8d2a00c5d69a990a94ed2ecc03961c39ec857b61
phon xay <majic42@hotmail.com>
2024-09-10 17:15:50 +1000

    first commit

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replace the element at the specified index in the list.
+
+    Parameters:
+    my_list (list): The list to modify
+    idx (int): The index at which to replace the element
+    element: The new element to insert
+
+    Returns:
+    list: The modified list if the index is within range, otherwise the original list
+    """
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return my_list
