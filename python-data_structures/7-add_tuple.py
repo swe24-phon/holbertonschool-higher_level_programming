03c02dbeaec0e93ed28c51962cda562167cac562
phon xay <majic42@hotmail.com>
2024-09-15 21:37:28 +1000

    fix style

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -16,16 +16,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) > 0:
         sum1 += tuple_a[0]
-
     if len(tuple_b) > 0:
         sum1 += tuple_b[0]
 
-
     if len(tuple_a) > 1:
         sum2 += tuple_a[1]
-
     if len(tuple_b) > 1:
         sum2 += tuple_b[1]
-
-
     return sum1, sum2
