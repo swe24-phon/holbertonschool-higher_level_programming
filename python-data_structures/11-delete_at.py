df10a4266db3eb5a63c6b0c1df79a60be6f533fe
phon xay <majic42@hotmail.com>
2024-09-15 22:31:02 +1000

    Fix style 2

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -7,7 +7,7 @@ def delete_at(my_list=[], idx=0):
     idx (int): The index of the element to delete.
     return:
     list: The updated list.
-    
+
     """
     if idx < 0 or idx >= len(my_list):
         return my_list
