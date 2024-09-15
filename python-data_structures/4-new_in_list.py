fdf7987f8d01e3e3805938e488ca83de85060ca1
phon xay <majic42@hotmail.com>
2024-09-15 19:56:21 +1000

    First commit

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """Insert a new element at a specific index in a list.
+
+    parameter:
+    my_list (list): The list to be modified.
+    idx (int): The index at which the new element will be inserted.
+    element (any): The new element to be inserted.
+
+    Returns:
+    list: The modified list.    
+    """
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list.insert(idx, element)
+    return my_list
