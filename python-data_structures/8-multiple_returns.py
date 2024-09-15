882de652f8929c72c1491fb049eff0a1c080d608
phon xay <majic42@hotmail.com>
2024-09-15 21:51:28 +1000

    First commit

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """
+    This function takes a sentence as input and returns a tuple containing the number of vowels and consonants
+    in the sentence.
+
+    parameter:
+    sentence (str): The input sentence.
+    return:
+    len(sentence): sentence length
+    sentence[0]: First character
+    """
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
+    
