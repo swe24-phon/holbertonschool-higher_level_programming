1a34b1dfa500078c6b437fe5a397b650088c744c
phon xay <majic42@hotmail.com>
2024-09-15 20:11:28 +1000

    Fix style2

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -9,7 +9,7 @@ def new_in_list(my_list, idx, element):
     element (any): The new element to be inserted.
 
     Returns:
-    list: The modified list.    
+    list: The modified list.
     """
     new_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
