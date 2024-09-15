53176c58145930024c9ba1e873fc5203da2cda45
phon xay <majic42@hotmail.com>
2024-09-15 19:59:37 +1000

    Fix logic

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -12,7 +12,7 @@ def new_in_list(my_list, idx, element):
     Returns:
     list: The modified list.    
     """
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         my_list.insert(idx, element)
