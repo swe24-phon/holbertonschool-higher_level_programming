8ac9d064e797c9bba42ba4804f9937c86e79cd71
phon xay <majic42@hotmail.com>
2024-09-16 14:00:15 +1000

    First commit

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix):
+    """Returns a new matrix where each element is the square of the corresponding element in the input matrix
+    parameters:
+    matrix (list): A 2D list of integers
+    Returns:
+    list: A 2D list of integers
+    """
+    return [[element ** 2 for element in row] for row in matrix]
