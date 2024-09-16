157e95fdc403f188155739c9b50c040965ade220
phon xay <majic42@hotmail.com>
2024-09-17 09:50:13 +1000

    upload

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    This function raises a TypeError when called.
+    parameters:
+    None
+    returns:
+    None
+    """
+    raise TypeError
