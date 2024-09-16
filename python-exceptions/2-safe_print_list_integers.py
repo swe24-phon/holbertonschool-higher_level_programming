22bc73dd966259120b3ee57c93ebf8834b26416a
phon xay <majic42@hotmail.com>
2024-09-17 01:11:01 +1000

    add raise

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -14,7 +14,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             continue
+        except IndexError:
+            print()
+            raise
     print()
     return count
