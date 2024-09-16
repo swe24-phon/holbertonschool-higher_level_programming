605bab020193c171650fc9fec7fb13f1f6590d61
phon xay <majic42@hotmail.com>
2024-09-17 09:54:32 +1000

    upload

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    This function raises an exception with a custom message.
+    parameters:
+    message (str): The message to be displayed in the exception.
+    """
+    raise Exception(message)
