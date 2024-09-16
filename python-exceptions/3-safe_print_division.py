37beac38a4631d9669e000e6fbe6ecff7d863314
phon xay <majic42@hotmail.com>
2024-09-17 01:23:16 +1000

    upload

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """
+    Divides a by b and prints the result, handling potential\
+        division by zero errors.
+    parameters:
+    a (int): The dividend.
+    b (int): The divisor.
+    return:
+    result:  The result of the division.
+    """
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        if result is not None:
+            print("Inside result: {}".format(result))
+        return result
