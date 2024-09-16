b4c62e4e2c7c66f25ca02a2fcb16137d6e8a2c7f
phon xay <majic42@hotmail.com>
2024-09-17 09:34:42 +1000

    add new exception

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -16,7 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         except ZeroDivisionError:
-            division by 0
+            print("division by 0")
         except TypeError:
             print("wrong type")
         finally:
