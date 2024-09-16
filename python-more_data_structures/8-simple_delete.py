1563e56bdf4bb762e30297fdb6f41ae9a33382f6
phon xay <majic42@hotmail.com>
2024-09-16 15:43:20 +1000

    first commit

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    """
+    Deletes a key from a dictionary.
+    parameters:
+    a_dictionary (dict): The dictionary to delete from.
+    key (str): The key to delete. Defaults to an empty string.
+    return:
+    dict: The dictionary with the key deleted.
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+        return a_dictionary
+    
