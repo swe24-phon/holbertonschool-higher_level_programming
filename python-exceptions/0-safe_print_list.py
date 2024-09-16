6b4c57cbf7b966140010f407d8c44175bf93a480
phon xay <majic42@hotmail.com>
2024-09-17 00:26:19 +1000

    modified

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+def i_count(lst):
+    """
+    This function counts the number of elements in a list.
+    parameters:
+    lst (list): The list to be counted
+    return:
+    int: The number of elements in the list
+    """
+    count = 0
+    for _ in lst:
+        count += 1
+    return count
+
 def safe_print_list(my_list=[], x=0):
     """
     This function prints a list of elements, but only if the list\
@@ -8,12 +21,12 @@ def safe_print_list(my_list=[], x=0):
     x (int): The minimum number of elements required in the list\
         to print it
     return:
-    None
+    int: The number of elements printed
     """
 
     try:
         count = 0
-        for i in range(min(x, len(my_list))):
+        for i in range(min(x, i_count(my_list))):
             print(my_list[i], end="")
             count += 1
         print()
