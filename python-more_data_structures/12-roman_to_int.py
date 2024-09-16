2ac165bb41ddc875892e4cc06a17e9308aaad71c
phon xay <majic42@hotmail.com>
2024-09-16 22:37:41 +1000

    add return 0 condition

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,11 +10,14 @@ def roman_to_int(roman_string):
     """
     roman_numeral_dict = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
     result = 0
-    for i in range(len(roman_string) - 1):
-        if roman_numeral_dict[roman_string[i]] < roman_numeral_dict[roman_string[i + 1]]:
-            result -= roman_numeral_dict[roman_string[i]]
-        else:
-            result += roman_numeral_dict[roman_string[i]]
-    result += roman_numeral_dict[roman_string[-1]]
+    if roman_string == {}:
+        return 0
+    else:
+        for i in range(len(roman_string) - 1):
+            if roman_numeral_dict[roman_string[i]] < roman_numeral_dict[roman_string[i + 1]]:
+                result -= roman_numeral_dict[roman_string[i]]
+            else:
+                result += roman_numeral_dict[roman_string[i]]
+        result += roman_numeral_dict[roman_string[-1]]
 
     return result
