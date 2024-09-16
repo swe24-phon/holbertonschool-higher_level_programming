4d30ea6e525898f1a5b1614d17de6cee96ed8722
phon xay <majic42@hotmail.com>
2024-09-16 20:15:55 +1000

    first commit

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """
+    Returns the best score if it exists in the dictionary, otherwise None
+    parameters:
+    a_dictionary (dict): The dictionary to be printed.
+    return:
+    None
+    """
+    if a_dictionary is None:
+        return None
+    elif not a_dictionary:
+        return None
+    else:
+        best_score = none
+        for key in a_dictionary:
+            if best_score < a_dictionary[key] or best_score is None:
+                best_score = a_dictionary[key]
+        return best_score]
+    
