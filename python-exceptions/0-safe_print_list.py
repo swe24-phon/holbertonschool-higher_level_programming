6d8bc1bd6f8d1552eae6213e91f6a73920f5d4b8
phon xay <majic42@hotmail.com>
2024-09-17 00:15:48 +1000

    change logic

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -13,7 +13,7 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         count = 0
-        for i in range(x):
+        for i in range(min(x, len(my_list))):
             print(my_list[i], end="")
             count += 1
         print()
