8399674090e5452ef5eb9160cf5debd383b9bfc9
phon xay <majic42@hotmail.com>
2024-09-17 09:39:11 +1000

    update finally

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -20,5 +20,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
         finally:
-            return result
+            if i == list_length - 1:
+                return result
+    return result
         
