cb6e61899bc9e1d99c2a06f0e2aca11d1067dff3
phon xay <majic42@hotmail.com>
2024-09-16 15:30:54 +1000

    first commit

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    parameters:
+    a_dictionary: key, value pairs
+    return
+    int: number of keys
+    """
+    return len(a_dictionary.keys())
