5934f8790b60920dc7df4c471e3790e9d7b85340
phon xay <majic42@hotmail.com>
2024-09-16 22:47:14 +1000

    add condition not a string or None

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,7 +10,8 @@ def roman_to_int(roman_string):
     """
     roman_numeral_dict = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
     result = 0
-    if roman_string == {}:
+    if not isinstance(roman_string, str) or\
+        roman_string is None:
         return 0
     else:
         for i in range(len(roman_string) - 1):
