b1e7e0988bfdcd1007c47e4478623b6001337886
phon xay <majic42@hotmail.com>
2024-09-17 01:35:52 +1000

    test new logic

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -12,11 +12,11 @@ def safe_print_division(a, b):
     """
     try:
         result = a / b
-    except ZeroDivisionError:
-        result = None
         print("Inside result: {}".format(result))
         print("{} / {} = {}".format(a, b, result))
+    except ZeroDivisionError:
+        result = None
     finally:
         if result is not None:
             print("Inside result: {}".format(result))
-        return result
+    return result
