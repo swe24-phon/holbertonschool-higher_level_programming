2570465b1363066522add78ee087044435536e18
phon xay <majic42@hotmail.com>
2024-09-16 20:49:50 +1000

    reduce lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    Multiply each element in a list by a given number.
-    parameters:
-    my_list: list of elements
-    number: multiplier
-    returns:
-    new_list: list of elements multiplied by the number
-    """
     return list(map(lambda x: x * number, my_list))
