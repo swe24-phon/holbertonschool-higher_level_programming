35ac3cec93418b7819007b1c8761212b3d2a5ce1
phon xay <majic42@hotmail.com>
2024-09-16 15:22:05 +1000

    First commit

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """
+    This function takes two sets as input and returns a new set\
+         containing only the elements that are present in either set_1 or\
+            set_2, 1 but not in set_2, and vice versa.
+    parameters:
+    set_1 (set): The first set.
+    set_2 (set): The second set.
+    returns:
+    set: A new set containing elements that are in set_1 but not in set_2
+
+    """
+    return set_1.union(set_2)
