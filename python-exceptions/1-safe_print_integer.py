a51619f6b216cfc024dcc3e3acd3ceeae920c0f3
phon xay <majic42@hotmail.com>
2024-09-17 00:41:52 +1000

    upload

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+    Prints an integer with "{:d}".format() and returns True if\
+        successful.
+    
+    Args:
+        value (any): The value to be printed as an integer.
+    
+    Returns:
+        bool: True if value is an integer and printed successfully,\
+            False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
+    
