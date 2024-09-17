1d3794a8dd42ada9b37893cd695f87c4ae44fb7f
phon xay <majic42@hotmail.com>
2024-09-18 01:25:07 +1000

    fix logic 2

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,19 +1,4 @@
-#!/usr/bin/python3
-"""
-This is the "Square"  module.
-This module provides a simple Square class.
-with initialize size.
-Size defaults to 0. Raise errors on invalid inputs.
-Methods Getter and Setter properties for size.
-Method area returns size of area of the square.
-Method my_print prints the square using "#".
-"""
-
-
 class Square:
-    """
-    define class for Square
-    """
     def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -21,15 +6,13 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
         if type(position) is not tuple or len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive\
-                            integers")
-        if not (isinstance(position[0], int) and\
-                isinstance(position[1], int)):
-            raise TypeError("position must be a tuple of 2\
-                            positive integers")        
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not(isinstance(position[0], int) and\
+               isinstance(position[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         if position[0] < 0 or position[1] < 0:
-            raise ValueError("position must be a tuple of 2 positive\
-                             integers")
+            raise ValueError\
+                ("position must be a tuple of 2 positive integers")
         self.__position = position
 
     def area(self):
@@ -55,8 +38,11 @@ class Square:
     def position(self, value):
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if not (isinstance(value[0], int) and isinstance(value[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
-            raise ValueError("position must be a tuple of 2 positive integers")
+            raise ValueError\
+                ("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
