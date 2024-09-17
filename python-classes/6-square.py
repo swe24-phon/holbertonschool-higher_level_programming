815c2034c76d8d6fcee2c6501c6961b0b4333ce3
phon xay <majic42@hotmail.com>
2024-09-18 01:19:58 +1000

    fix logic

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -23,6 +23,10 @@ class Square:
         if type(position) is not tuple or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive\
                             integers")
+        if not (isinstance(position[0], int) and\
+                isinstance(position[1], int)):
+            raise TypeError("position must be a tuple of 2\
+                            positive integers")        
         if position[0] < 0 or position[1] < 0:
             raise ValueError("position must be a tuple of 2 positive\
                              integers")
