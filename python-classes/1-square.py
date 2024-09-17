cf20da7ccff11be327e19202a7c4f052bd49e637
phon xay <majic42@hotmail.com>
2024-09-17 20:07:33 +1000

    upload

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the "Square"  module.
+This module provides a simple Square class.
+"""
+
+
+class Square:
+    """
+    define class for Square
+    """
+    def __init__(self, size):
+        self.__size = size
+    pass
