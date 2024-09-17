1549881892bb594cb22efbb5cd75bec8419c7779
phon xay <majic42@hotmail.com>
2024-09-17 20:49:36 +1000

    fix style

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,9 +20,11 @@ class Square:
 
     def area(self):
         return self.__size ** 2
+    
     @property
     def size(self):
         return self.__size
+    
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -30,4 +32,3 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
