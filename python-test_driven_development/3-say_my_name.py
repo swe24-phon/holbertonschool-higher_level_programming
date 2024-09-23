be118195b45131a5a2a6ac5bb48ff8bb9a218cea
phon xay <majic42@hotmail.com>
2024-09-24 00:21:18 +1000

    restore

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -20,13 +20,9 @@ def say_my_name(first_name, last_name=""):
         TypeError: if the type is not string
 
     """
-
     if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string, not {}".format(type\
-                                            (first_name).__name__))
+        raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string, not {}".format(type\
-                                            (last_name).__name__))
+        raise TypeError("last_name must be a string")
     
     print(f"My name is {first_name} {last_name}".strip())
-    
