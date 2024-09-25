91f5554e058acf7544b346c3e8beffd1c8cbf383
phon xay <majic42@hotmail.com>
2024-09-25 19:41:19 +1000

    update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,4 @@
-#!/usr/bin/python3
-
-"""
-This module provides a function that divides all elements of a matrix.
-"""
-
-
-def matrix_divided(matrix, div):
+def matrix_divided(matrix=None, div=None):
     """
     Divides all elements of a matrix by a given divisor.
 
@@ -28,24 +21,24 @@ def matrix_divided(matrix, div):
     """
     if matrix is None or div is None:
         raise TypeError("matrix_divided() missing 1 required"
-                        " positional argument")    
-    if div == float('inf'):
-        return [[0.0 for _ in row] for row in matrix]    
+                        " positional argument")
     if not isinstance(matrix, list) or not matrix:
         raise TypeError("matrix must be a non-empty list of lists")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+    if div == float('inf'):
+        return [[0.0 for _ in row] for row in matrix]
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of"
                         " integers/floats")
-    if not all(isinstance(elem, (int, float)) for row in matrix for elem\
-               in row):
+    if not all(isinstance(elem, (int, float)) for row in matrix for\
+               elem in row):
         raise TypeError("matrix must be a matrix (list of lists) of"
                         " integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = [[round(float(elem) / div, 2) if elem != 0 else\
-                   elem for elem in row] for row in matrix]
+    new_matrix = [[round(float(elem) / div, 2) if elem != 0 else elem for\
+                   elem in row] for row in matrix]
     return new_matrix
