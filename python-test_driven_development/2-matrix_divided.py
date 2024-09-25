23e9a557ae169da27d4f8f8984be2992003d8219
phon xay <majic42@hotmail.com>
2024-09-25 19:48:31 +1000

    update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -26,9 +26,14 @@ def matrix_divided(matrix=None, div=None):
         ZeroDivisionError: If div is zero.
 
     """
+
     if matrix is None or div is None:
-        raise TypeError("matrix_divided() missing 1 required"
-                        " positional argument")
+        if matrix is None and div is None:
+            raise TypeError("matrix_divided() missing 2 required"
+                            " positional arguments")
+        else:
+            raise TypeError("matrix_divided() missing 1 required"
+                            " positional argument")
     if not isinstance(matrix, list) or not matrix:
         raise TypeError("matrix must be a non-empty list of lists")
     if not isinstance(div, (int, float)):
@@ -36,12 +41,12 @@ def matrix_divided(matrix=None, div=None):
     if div == float('inf'):
         return [[0.0 for _ in row] for row in matrix]
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of"
-                        " integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of\
+                        integers/floats")
     if not all(isinstance(elem, (int, float)) for row in matrix for\
                elem in row):
-        raise TypeError("matrix must be a matrix (list of lists) of"
-                        " integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of\
+                        integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
