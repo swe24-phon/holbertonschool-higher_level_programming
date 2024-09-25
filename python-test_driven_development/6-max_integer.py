929068c7071016fad99a9904a97b4759160155aa
phon xay <majic42@hotmail.com>
2024-09-25 18:04:03 +1000

    Upload

---
+++ python-test_driven_development/6-max_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module is composed by a function that 
+finding the max integer in a list
+
+"""
+
+
+def max_integer(lst=[]):
+    """
+    Returns the maximum integer in the input list.
+
+    PARAMETERS:
+        lst (list, optional): The input list of integers. Defaults to an empty list.
+
+    Returns:
+        int: The maximum integer in the input list.
+
+    Raises:
+        ValueError: If the input list is empty.
+        TypeError: If the input list contains non-integer elements.
+    """
+    if not lst:
+        raise ValueError("Input list is empty")
+
+    for element in lst:
+        if not isinstance(element, int):
+            raise TypeError("Input list contains non-integer elements")
+
+    return max(lst)
---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([])
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertRaises(ValueError, max_integer, [])
+
+    def test_single_element_list(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_multiple_elements_list(self):
+        self.assertEqual(max_integer([3, 1, 4, 2]), 4)
+
+    def test_list_with_negative_numbers(self):
+        self.assertEqual(max_integer([-3, -1, -4, -2]), -1)
+
+    def test_list_with_duplicates(self):
+        self.assertEqual(max_integer([3, 3, 4, 2]), 4)
+
+    def test_non_integer_elements(self):
+        with self.assertRaises(TypeError):
+            max_integer([3, 'a', 4, 2])
+
+if __name__ == '__main__':
+    unittest.main()
