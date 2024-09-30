b47db793cedd5967df37b0494cabafe024c9d999
phon xay <majic42@hotmail.com>
2024-09-30 16:55:55 +1000

    fix style

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,13 +6,13 @@ This module contains a class Mylist that inherits from list
 
 class MyList(list):
     """
-    This class inherits from list    
+    This class inherits from list
     """
     def print_sorted(self):
         """
         This method prints the sorted list
         Parameters:
-        self: the list of integers        
+        self: the list of integers
         """
 
         print(sorted(self))
