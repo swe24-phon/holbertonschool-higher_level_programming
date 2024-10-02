d086c1a88734ff81fdfd5e6223a329208f69585d
phon xay <majic42@hotmail.com>
2024-10-03 09:11:10 +1000

    upload

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module def is_same_class compare class to instance
+of class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Check if object is exactly an instance of the specified class"""
+    return type(obj) == a_class
