63efc06d8a3fb52e7944982eb1124863438f6c07
phon xay <majic42@hotmail.com>
2024-10-03 09:28:18 +1000

    change to is

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,4 +7,4 @@ of class
 
 def is_same_class(obj, a_class):
     """Check if object is exactly an instance of the specified class"""
-    return type(obj) == a_class
+    return type(obj) is a_class
