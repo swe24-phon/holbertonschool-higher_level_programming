5719169f5e02e5a236ab09fe0b1d1bde4289c437
phon xay <majic42@hotmail.com>
2024-10-03 09:58:52 +1000

    fix style

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,6 +8,7 @@ The module contains the following method:
  a class that inherited from the specified class.
 """
 
+
 def inherits_from(obj, a_class):
     """Check if object is an instance of a class that inherited
     from the specified class"""
