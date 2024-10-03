a168d1bd4094ccaf854afcbcf5129de5d1a59082
phon xay <majic42@hotmail.com>
2024-10-03 10:12:45 +1000

    upload

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module provides class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry
+        
+    """
+    def area(self):
+        """Method raises an Exception"""
+        raise Exception("area() is not implemented")
+    
