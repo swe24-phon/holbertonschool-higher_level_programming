f5ef8e6e3f521b483334f4fa04cad9ede70529ea
phon xay <majic42@hotmail.com>
2024-10-03 17:30:58 +1000

    change to use import of parent

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,25 +3,8 @@
 This module provides class BaseGeometry.
 """
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class BaseGeometry:
-    """class BaseGeometry
-
-    """
-    def area(self):
-        """Method raises an Exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates a value.
-        If value is not integer, raise a TypeError
-        If value <= 0, raise a Value error
-        """
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """class Rectangle
