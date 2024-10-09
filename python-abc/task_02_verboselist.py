5901c5d72b0dc1dd2afcd32d4b7a42204ac74153
phon xay <majic42@hotmail.com>
2024-10-09 18:41:33 +1100

    remove abstract classes

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -5,30 +5,23 @@ This Python code demonstrates the concept of inheritance
 to define a derived classes 'VerboseList'.
 """
 
-
-import math
-from abc import ABC, abstractmethod
-"""
-
-"""
-
 class VerboseList(list):
-    @abstractmethod
+
     def append(self, item):
         super.append()
         print(f"Added [{item}] to the list.")
 
-    @abstractmethod
+
     def extend(self, iterable):
         super().extend(items)
         print(f"Extended the list with [{len(items)}] items.")
 
-    @abstractmethod
+
     def remove(self, item):
         super().remove(item)
         print(f"Removed [{item}] from the list.")
 
-    @abstractmethod
+
     def pop(self):
         item = super().pop(index)
         print(f"Popped [{item}] from the list.")
