3fd0083a069d8fa7e217631f9ebf0ae9cc7e4e7e
phon xay <majic42@hotmail.com>
2024-10-09 18:23:45 +1100

    Upload

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+This Python code demonstrates the concept of inheritance
+to define a derived classes 'VerboseList'.
+"""
+
+
+import math
+from abc import ABC, abstractmethod
+"""
+
+"""
+
+class VerboseList(List):
+    @abstractmethod
+    def append(self, item):
+        super.append()
+        print(f"Added [{item}] to the list.")
+
+    @abstractmethod
+    def extend(self, iterable):
+        super().extend(items)
+        print(f"Extended the list with [{len(items)}] items.")
+
+    @abstractmethod
+    def remove(self, item):
+        super().remove(item)
+        print(f"Removed [{item}] from the list.")
+
+    @abstractmethod
+    def pop(self):
+        item = super().pop(index)
+        print(f"Popped [{item}] from the list.")
+        return item
