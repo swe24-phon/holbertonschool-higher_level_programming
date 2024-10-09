d81ac42e7775c2a0727cc48193bfedd92423be25
phon xay <majic42@hotmail.com>
2024-10-09 18:55:34 +1100

    add -1

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -25,7 +25,7 @@ class VerboseList(list):
         print(f"Removed [{item}] from the list.")
 
 
-    def pop(self, index):
+    def pop(self, index= -1):
         item = super().pop(index)
         print(f"Popped [{item}] from the list.")
         return item
