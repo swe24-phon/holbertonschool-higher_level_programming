e4849c71b50782a905d68b6ab9451abd429b4890
phon xay <majic@hotmail.com>
2024-10-27 12:42:01 +0000

    upload

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -10,17 +10,19 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-"""
+
+    """
     Turning python dictionary into json and save to file
-"""    
+    """    
 
     with open(filename, 'w') as file:
         json.dump(data,file)
 
 def load_and_deserialize(filename):
-"""
+
+    """
     Reversing json file into python dictionary
-"""    
+    """    
 
     with open(filename, 'r') as file:
         return json.load(file)
