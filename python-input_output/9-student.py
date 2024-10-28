62d565f675d47e8809b30146253171ac870f4c06
phon xay <majic42@hotmail.com>
2024-10-28 16:13:17 +1100

    Refactor Student class to make it serializable

---
+++ python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student
+make it serialisable
+"""
+
+
+import json
+
+
+class Student:
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ return dictionary description with simple data structure """
+        return self.__dict__
