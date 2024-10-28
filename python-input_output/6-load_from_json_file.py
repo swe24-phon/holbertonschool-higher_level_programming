d4fc6c709b1b552df065facf228c54378b44f320
phon xay <majic42@hotmail.com>
2024-10-28 15:08:04 +1100

    Refactor load_from_json_file function to return the loaded JSON object

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -11,4 +11,4 @@ def load_from_json_file(filename):
     """ load from JSON file as python object """
 
     with open(filename, 'r', encoding='utf-8') as file:
-        json.load(file)
+        return json.load(file)
