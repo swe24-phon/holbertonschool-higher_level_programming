7cd8af48e667405d739449aad0a49ecd5b68dae4
phon xay <majic42@hotmail.com>
2024-10-28 14:25:41 +1100

    upload

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Convert an object to a JSON string
+"""
+import json
+
+def to_json_string(my_obj):
+    """Convert an object to a JSON string."""
+    return json.dumps(my_obj)
