9c671e30295796074f89398dd3ce4a04d39c781b
phon xay <majic42@hotmail.com>
2024-11-06 16:04:05 +1100

    Refactor 0-select_states.py to improve docstrings and enhance output formatting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 import MySQLdb
-"""Fetching all rows from the states table in the database hbtn_0e_0_usa"""
+"""
+Fetching all rows from the states table in the database hbtn_0e_0_usa
+"""
 
 
 if __name__ == "__main__":
+    """Verify that the module is being run directly"""
+
     conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root",\
                         db="hbtn_0e_0_usa", charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        id, name = row
+        print(id, name)
     cur.close()
     conn.close()
