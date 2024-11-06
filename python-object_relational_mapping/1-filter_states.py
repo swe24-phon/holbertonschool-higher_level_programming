849507310ed4e5d8c6b59a8d10c062215e7948e2
phon xay <majic42@hotmail.com>
2024-11-06 18:14:24 +1100

    Add 1-filter_states.py to fetch and filter states from the database

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Fetching all rows from the states table in the database hbtn_0e_0_usa and filtering by name"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
