249214329b3397b126c413f422ad65d4fefdc2da
phon xay <majic42@hotmail.com>
2024-11-06 16:18:55 +1100

    Refactor 0-select_states.py to implement StateFetcher class for improved database interaction and code organization

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,20 +2,41 @@
 import MySQLdb
 
 """
-
 This module fetches all rows from the states table in the database hbtn_0e_0_usa.
 """
 
+
+class StateFetcher:
+    """
+    A class to fetch states from the database.
+    """
+
+    def __init__(self, host, user, password, db):
+        """
+        Initializes the database connection.
+        """
+        self.conn = MySQLdb.connect(host=host, user=user, passwd=password, db=db, charset="utf8")
+        self.cur = self.conn.cursor()
+
+    def fetch_states(self):
+        """
+        Fetches all states from the states table.
+        """
+        self.cur.execute("SELECT * FROM `states` ORDER BY `id` ASC")
+        return self.cur.fetchall()
+
+    def close(self):
+        """
+        Closes the database connection.
+        """
+        self.cur.close()
+        self.conn.close()
+
 if __name__ == "__main__":
     """Verify that the module is being run directly"""
-
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root",\
-                        db="hbtn_0e_0_usa", charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY `id` ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        id, name = row
+    fetcher = StateFetcher("localhost", "root", "root", "hbtn_0e_0_usa")
+    states = fetcher.fetch_states()
+    for state in states:
+        id, name = state
         print(id, name)
-    cur.close()
-    conn.close()
+    fetcher.close()
