31664305fda9e7f901ad79a0ab6c3ef3cfd6a3c0
phon xay <majic42@hotmail.com>
2025-03-17 06:23:01 +1100

    First commit

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Lists all City objects from the database hbtn_0e_101_usa"""
 
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
