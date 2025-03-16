07874397de44d63475adbb454ec6726b3baef112
phon xay <majic42@hotmail.com>
2025-03-16 18:23:26 +1100

    First commit

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies 2 matrices by using the module NumPy
+
+    Args:
+        m_a (list of lists): first matrix
+        m_b (list of lists): second matrix
+
+    Returns:
+        list of lists: result of the multiplication
+    """
+
+    return np.matmul(m_a, m_b).tolist()
