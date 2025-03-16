be763c18c68d0aa36b1c1eae3a5acd28bcc9c0f4
phon xay <majic42@hotmail.com>
2025-03-17 02:54:10 +1100

    First commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 class State(Base):
     """Represents a state for a MySQL database"""
-    
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
