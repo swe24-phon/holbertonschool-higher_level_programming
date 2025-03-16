bee78f21f7036f6c731dde2786ae18e44d6d045d
phon xay <majic42@hotmail.com>
2025-03-16 23:42:02 +1100

    First commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,5 @@
 --- create database and  user
 CREATE DATABASE hbtn_0d_2 if not exists;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
