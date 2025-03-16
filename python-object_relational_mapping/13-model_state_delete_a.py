9628f28c99885146c51cf95de68b6d38c59de1f5
phon xay <majic42@hotmail.com>
2025-03-17 05:13:44 +1100

    First commit

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -27,7 +27,9 @@ if __name__ == "__main__":
 
     with Session(engine) as session:
         # Query and delete State objects where the name contains 'a'
-        states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+        states_to_delete = session.query(State).filter(
+            State.name.like('%a%')
+        ).all()
         for state in states_to_delete:
             session.delete(state)
         session.commit()
