9596aabcc69c6a1ad7a2b891b2d3dda70501ebd8
phon xay <majic42@hotmail.com>
2025-03-18 10:24:49 +1100

    First commit

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -21,7 +21,7 @@ def contact():
 def items():
     with open('items.json', 'r') as f:
         data = json.load(f)
-    items_list = data['items']
+    items_list = data.get['items' , []]
     return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
